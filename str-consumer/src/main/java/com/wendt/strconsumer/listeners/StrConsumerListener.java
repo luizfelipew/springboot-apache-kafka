843d43188cdfc512d282e03118d1a69bf5b9c91a
Luiz Felipe <luizfelipewendt@gmail.com>
2022-11-09 11:53:12 -0300

    record interceptor

--- str-consumer/src/main/java/com/wendt/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/wendt/strconsumer/listeners/StrConsumerListener.java
@@ -2,6 +2,7 @@ package com.wendt.strconsumer.listeners;
 
 import com.wendt.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -18,7 +19,7 @@ public class StrConsumerListener {
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(final String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
