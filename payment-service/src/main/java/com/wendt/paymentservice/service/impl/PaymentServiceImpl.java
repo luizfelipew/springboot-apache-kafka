f77c3f849cb2d423f5ad1071fb18c4f7a8e9ee79
Luiz Felipe <luizfelipewendt@gmail.com>
2022-11-09 18:21:31 -0300

    added producer config to payment

--- payment-service/src/main/java/com/wendt/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/wendt/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,16 +2,29 @@ package com.wendt.paymentservice.service.impl;
 
 import com.wendt.paymentservice.model.Payment;
 import com.wendt.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(final Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento ... ");
+        kafkaTemplate.send("payment-topic", payment);
     }
 
 }
